273c99d67fb25663b05cb4e58ef3fc910292012c
LeticiAz <leticia.ig.azevedo@gmail.com>
2025-08-11 12:22:06 +0100

    Add js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-let currentTime = new Date();
+function updateWorldClock() {
+    //Los Angeles
+    let losAngelesElement = document.getElementById('los-angeles');
+    let losAngelesDateElement = losAngelesElement.querySelector('.date');
+    let losAngelesTimeElement = losAngelesElement.querySelector('.time');
+    losAngelesDateElement.innerHTML = moment().format('MMMM Do YYYY');
+    losAngelesTimeElement.innerHTML = moment.tz('America/Los_Angeles').format('h:mm:ss [<small>]A[</small>]');
+
+    // London
+    let londonElement = document.getElementById('london');
+    let londonDateElement = londonElement.querySelector('.date');
+    let londonTimeElement = londonElement.querySelector('.time');
+    londonDateElement.innerHTML = moment().format('MMMM Do YYYY');
+    londonTimeElement.innerHTML = moment.tz('Europe/London').format('h:mm:ss [<small>]A[</small>]');
+}
+
+updateWorldClock(); // Initial call to set the time immediately
+setInterval(updateWorldClock, 1000);
