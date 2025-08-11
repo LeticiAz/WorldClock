de2177f54f4c0f8c0abf000897c11af05a7a5afc
LeticiAz <leticia.ig.azevedo@gmail.com>
2025-08-11 15:05:54 +0100

    Add current location and correction on how to get city name

--- src/index.js
+++ src/index.js
@@ -23,7 +23,10 @@ function updateWorldClock() {
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
-    let cityName = event.target.options[event.target.selectedIndex].text;
+    if (cityTimeZone === 'current') {
+        cityTimeZone = moment.tz.guess();
+    };
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityDate = moment().tz(cityTimeZone);
 
     let citiesElement = document.getElementById('cities');
